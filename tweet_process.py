caefd7e3173be802dabd746f898898e7b6644666
Josh <joshuaj004@gmail.com>
2018-10-26 20:32:47 -0400

    Added command line flags

--- tweet_process.py
+++ tweet_process.py
@@ -1,24 +1,52 @@
 from shutil import copyfile
-import fileinput
+import fileinput, argparse
 
 def main():
     print("Copying file")
     copyfile("cropped_tweet.html", "cropped_tweet_temp.html")
     print("replacing text")
-    replace_dict = replacement_vars()
+    replace_dict = process_flags()
     replace_text(replace_dict)
     print("Done")
 
+def process_flags():
+    parser = argparse.ArgumentParser(description='Create your own tweet based off of the iconic "Thank you Kanye, very cool!" tweet.')
+    parser.add_argument('-itn --innertweetname', metavar='metavar', type=str, nargs='?',
+                    help='Replaces the inner tweet name',
+                    default='ye')
 
-def replacement_vars():
+    parser.add_argument('-ith --innertweethandle', metavar='metavar', type=str, nargs='?',
+                    help='Replaces the inner tweet handle',
+                    default='kanyewest')
+
+    parser.add_argument('-itc --innertweetcontents', metavar='metavar', type=str, nargs='?',
+                    help='Replaces the inner tweet contents',
+                    default='tiger blood or something')
+
+    parser.add_argument('-otn --outertweetname', metavar='metavar', type=str, nargs='?',
+                    help='Replaces the outer tweet name',
+                    default='Donald J. Trump')
+
+    parser.add_argument('-oth --outertweethandle', metavar='metavar', type=str, nargs='?',
+                    help='Replaces the outer tweet handle',
+                    default='realDonaldTrump')
+
+    parser.add_argument('-otc --outertweetcontents', metavar='metavar', type=str, nargs='?',
+                    help='Replaces the outer tweet contents',
+                    default='Thank you {{INNERTWEETNAME}}, very cool!"')
+
+    args = parser.parse_args()
+
     replace_dict = {
-        "{{INNERTWEETNAME}}": "ye",
-        "{{INNERTWEETHANDLE}}": "kanyewest",
-        "{{INNERTWEETCONTENTS}}": "tiger blood or something",
-        "{{OUTERTWEETNAME}}": "Donald J. Trump",
-        "{{OUTERTWEETHANDLE}}": "realDonaldTrump",
-        "{{OUTERTWEETCONTENTS}}": "Thank you Kanye, very cool!",
+        "{{OUTERTWEETNAME}}": vars(args)['otn __outertweetname'],
+        "{{OUTERTWEETHANDLE}}": vars(args)['oth __outertweethandle'],
+        "{{OUTERTWEETCONTENTS}}": vars(args)['otc __outertweetcontents'],
+        "{{INNERTWEETNAME}}": vars(args)['itn __innertweetname'],
+        "{{INNERTWEETHANDLE}}": vars(args)['ith __innertweethandle'],
+        "{{INNERTWEETCONTENTS}}": vars(args)['itc __innertweetcontents']
     }
+
+    print(vars(args))
     return replace_dict
 
 def replace_text(replace_dict):
